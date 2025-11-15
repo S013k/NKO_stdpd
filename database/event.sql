48434618a0f781b3728f04a88c04145629ed2af5
Aleksandr Sofronov <sofronov-ae@qavm-f9b691f5.qemu>
2025-11-15 14:29:21 +0300

    change schema and add data

--- database/event.sql
+++ database/event.sql
@@ -1,10 +1,26 @@
+CREATE TABLE IF NOT EXISTS events_categories (
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS events_categories_link (
+    events_id BIGINT NOT NULL,
+    category_id SMALLINT NOT NULL,
+    PRIMARY KEY (events_id, category_id),
+    FOREIGN KEY (events_id) REFERENCES events(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES events_categories(id) ON DELETE RESTRICT
+);
+
 CREATE TABLE IF NOT EXISTS events (
     id BIGSERIAL PRIMARY KEY,
     nko_id BIGINT NOT NULL REFERENCES nko(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
-    type VARCHAR(100),
+    description TEXT,
     address TEXT, -- адрес
+    picture TEXT,
     coords POINT, -- координаты (x=lon, y=lat)
     starts_at TIMESTAMPTZ,
+    finish_at TIMESTAMPTZ,
     created_at TIMESTAMPTZ DEFAULT now()
 );
--- database/nko.sql
+++ database/nko.sql
@@ -1,11 +1,24 @@
+CREATE TABLE IF NOT EXISTS nko_categories (
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS nko_categories_link (
+    nko_id BIGINT NOT NULL,
+    category_id SMALLINT NOT NULL,
+    PRIMARY KEY (nko_id, category_id),
+    FOREIGN KEY (nko_id) REFERENCES nko(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES nko_categories(id) ON DELETE RESTRICT
+);
+
 CREATE TABLE IF NOT EXISTS nko (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    category VARCHAR(100),
     description TEXT,
-    url TEXT,
+    url TEXT, -- сайт нко
     logo TEXT,
-    address TEXT, -- адрес
-    coords POINT, -- координаты (x=lon, y=lat)
+    address TEXT NOT NULL, -- адрес
+    coords POINT NOT NULL, -- координаты (x=lon, y=lat)
     created_at TIMESTAMPTZ DEFAULT now()
 );
